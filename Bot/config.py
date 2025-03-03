6012ab7d2b6d9a950bde67647d4bd49d0c5d927a
unknown <Anwender@3127619890-603332205-307314686.3127619890-603332205-307314686>
2025-03-03 14:26:39 +0100

    Updated config

--- Bot/config.py
+++ Bot/config.py
@@ -6,11 +6,12 @@ TOKEN = os.getenv("MTM0NjA5MzMwMDM2MDQxNzM0MQ.GFqWag.RpO08hbYOCeZGyJ5fPEs-Fikylb
 # Bot Einstellungen
 COMMAND_PREFIX = "/"  # Slash-Commands für modernen Discord-Support
 
-# Liste ALLER bekannten Bildformate (Upload & Ziel-Format)
+# Unterstützte Bildformate (Upload & Ziel-Format)
 ALLOWED_FORMATS = [
-    "jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp", "ico", "svg", "psd", "ai", "eps", "pdf",
-    "heic", "raw", "nef", "cr2", "orf", "arw", "dng", "rw2", "raf", "sr2", "pef", "x3f", "dds",
-    "jp2", "jxr", "hdr", "exr", "pcx", "tga", "xcf", "indd", "cdr", "dwg", "skp", "webp"
+    "jpg", "jpeg", "png", "gif", "bmp", "tiff", "webp", "ico", "svg",
+    "psd", "ai", "eps", "pdf", "heic", "raw", "nef", "cr2", "orf", "arw",
+    "dng", "rw2", "raf", "sr2", "pef", "x3f", "dds", "jp2", "jxr", "hdr",
+    "exr", "pcx", "tga", "xcf", "indd", "cdr", "dwg", "skp"
 ]
 
 # Maximale Anzahl an Dateien pro Anfrage
